4d274cf9b78f4a0720ce17a00b2300c58674501f
Guilherme Henrique <guih-_-carvalho@hotmail.com>
2019-04-04 09:00:10 -0300

    First update: sum added

---
+++ RestWithASPNET/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstnumber, string secondNumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondNumber))
+            {
+                var sum = Convert.ToDecimal(firstnumber) + Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input.");
+        }
+
+        //Comentado. Existe a função Convert.To
+
+        //private decimal ConvertToDecimal(string number)
+        //{
+        //    decimal decimalValue;
+        //    if (decimal.TryParse(number, out decimalValue))
+        //    {
+        //        return decimalValue;
+        //    }
+        //    return 0;
+        //}
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
