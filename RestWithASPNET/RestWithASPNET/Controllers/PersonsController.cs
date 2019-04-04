f84a145f9ee17d504d4f530f77d2ecbb90aac0f4
Guilherme Henrique <guih-_-carvalho@hotmail.com>
2019-04-04 13:16:01 -0300

    Second ver.

--- RestWithASPNET/RestWithASPNET/Controllers/PersonsController.cs
+++ RestWithASPNET/RestWithASPNET/Controllers/PersonsController.cs
@@ -1,44 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET.Model;
+using RestWithASPNET.Services.Implementation;
 
 namespace RestWithASPNET.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonsController : Controller
     {
+
+        private IPersonService _personService;
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_personService.FindAll());
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var person = _personService.FindById(id);
+            if (person == null)
+                return NotFound();
+            return Ok(person);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]Person person)
         {
+            if (person == null)
+                return BadRequest();
+            return new ObjectResult(_personService.Create(person));
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]Person person)
         {
+            if (person == null)
+                return BadRequest();
+            return new ObjectResult(_personService.Update(person));
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            _personService.Delete(id);
+            return NoContent();
         }
     }
 }
--- RestWithASPNET/RestWithASPNET/Startup.cs
+++ RestWithASPNET/RestWithASPNET/Startup.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Services.Implementation;
 
 namespace RestWithASPNET
 {
@@ -23,7 +26,11 @@ namespace RestWithASPNET
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
             services.AddMvc();
+            services.AddApiVersioning();
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
